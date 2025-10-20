f5586d5e2f53753269e488f7067c21f875a26107
agkayster <ejike.chiboka@gmail.com>
2025-10-20 16:59:03 +0100

    feat: add user authentication routes and integrate signup controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,14 @@
+import { signup } from '#controllers/auth.controller.js';
 import express from 'express';
 
 /* router allows you to create routes */
 const router = express.Router();
 
-router.post('/sign-up', (req, res) => {
-  // Handle user sign-up logic here
-  res.status(201).send('POST /api/auth/sign-up response');
-});
+// router.post('/sign-up', (req, res) => {
+//   // Handle user sign-up logic here
+//   res.status(201).send('POST /api/auth/sign-up response');
+// });
+router.post('/sign-up', signup);
 
 router.post('/sign-in', (req, res) => {
   // Handle user sign-up logic here
